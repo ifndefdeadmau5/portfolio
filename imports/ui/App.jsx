7fefc1476a266f6148eac47f79948e9557b78882
Trevor <ifndefdeadmau5@gmail.com>
2017-02-04 18:07:01 +0900

    Initial commit

---
+++ imports/ui/App.jsx
@@ -0,0 +1,22 @@
+import React, { Component } from 'react';
+
+// App component - represents the whole app
+export default class App extends Component {
+
+  render() {
+    return (
+      <div className="container">
+        <header>
+          <h1>Me me me</h1>
+        </header>
+
+        <ul>
+          <li>Trevor</li>
+          <li>27</li>
+          <li>Male</li>
+          <li>Software Engineer</li>
+        </ul>
+      </div>
+    );
+  }
+}
