4d2c08c13c035bb332eb9924b7dc4d6277443359
Trevor <ifndefdeadmau5@gmail.com>
2017-02-06 19:27:56 +0900

    Enable submit message when user press ‘enter’

---
+++ client/component/ChatRoom.jsx
@@ -0,0 +1,72 @@
+import React, { Component } from 'react';
+import firebase from 'firebase';
+
+export default class ChatRoom extends Component {
+
+  constructor(props) {
+    super(props);
+    this.updateMessage = this.updateMessage.bind(this);
+    this.submitMessage = this.submitMessage.bind(this);
+    this.state = {
+      messages: [],
+    };
+
+    const config = {
+      apiKey: 'AIzaSyB4pOsn6qm1fdgkwaQEel1KDkZrlh_J_MY',
+      authDomain: 'trevorme-5b2c4.firebaseapp.com',
+      databaseURL: 'https://trevorme-5b2c4.firebaseio.com',
+      storageBucket: 'trevorme-5b2c4.appspot.com',
+      messagingSenderId: '759398557086',
+    };
+
+    firebase.initializeApp(config);
+  }
+
+  componentDidMount() {
+    firebase.database().ref('messages/').on('value', (snapshot) => {
+      const currentMessages = snapshot.val();
+      const list = Object.assign([], currentMessages);
+
+      if (currentMessages != null) {
+        this.setState({
+          messages: list,
+        }, () => console.log(this.state.messages));
+      }
+    });
+  }
+
+  updateMessage(event) {
+    this.setState({
+      message: event.target.value,
+    });
+  }
+
+  submitMessage() {
+    const nextMessage = {
+      id: this.state.messages.length,
+      text: this.state.message,
+    };
+
+    firebase.database().ref(`messages/${nextMessage.id}`).set(nextMessage);
+  }
+
+  render() {
+    return (
+      <div>
+        <ol>
+          {this.state.messages && this.state.messages.map(message =>
+            (
+              <li key={message.id}>{message.text}</li>
+            ))}
+        </ol>
+        <input
+          onKeyDown={(event) => {
+            if (event.keyCode === 13) {
+              this.submitMessage();
+            }
+          }} onChange={this.updateMessage} type="text" placeholder="Message" />
+        <button onClick={this.submitMessage}>Send</button>
+      </div>
+    );
+  }
+}
