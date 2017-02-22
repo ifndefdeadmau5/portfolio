99091acfaefcef93362e7c0fef22c8688d02d20c
Trevor <ifndefdeadmau5@gmail.com>
2017-02-22 20:09:32 +0900

    Update folder structure(Apply deleted files)

--- imports/ui/components/Button.jsx
+++ imports/ui/components/Button.jsx
@@ -2,8 +2,8 @@ import styled from 'styled-components';
 
 const Button = styled.button`
    /* Adapt the colors based on primary prop */
-  background: ${props => props.primary ? 'black' : 'white'};
-  color: ${props => props.primary ? 'white' : 'black'};
+  background: ${ props => props.primary ? 'black' : 'white' };
+  color: ${ props => props.primary ? 'white' : 'black' };
 
   font-size: 1em;
   padding: 0.25em 1em;
